206e8d4a0a409633fe86ed708ebbfc46b7781c89
Jason <66883665+json20@users.noreply.github.com>
2021-06-04 23:38:36 +0800

    Created book , bookinstance & genre schemas

--- models/author.js
+++ models/author.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema (
     {
-        first_name = {type: String, required: true, maxLength: 100},
-        family_name = {type: String, required: true, maxLength: 100},
+        first_name: {type: String, required: true, maxLength: 100},
+        family_name: {type: String, required: true, maxLength: 100},
         date_of_birth: {type: Date},
         date_of_death: {type: Date},
     }
